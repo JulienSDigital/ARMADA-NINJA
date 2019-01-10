956aaa1cd7001e6fbe90a21df290e197b8701cb9
GabinH <gabin.hiairrassary@hotmail.fr>
2019-01-10 11:43:23 +0100

    adding storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+import { NgModule, ErrorHandler } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { IonicApp, IonicModule, IonicErrorHandler } from "ionic-angular";
+import { MyApp } from "./app.component";
 
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
@@ -19,13 +19,14 @@ import { SplashScreen } from "@ionic-native/splash-screen";
 import { MapPage } from "../pages/map/map";
 
 import { Camera } from "@ionic-native/camera";
- import { Geolocation } from "@ionic-native/geolocation";
+import { Geolocation } from "@ionic-native/geolocation";
 import { listSportPage } from "../pages/listSport/listSport";
 import { surfPage } from "../pages/surf/surf";
 import { skiPage } from "../pages/Ski/ski";
 import { randoPage } from "../pages/rando/rando";
-import { GoogleMaps } from '@ionic-native/google-maps';
+import { GoogleMaps } from "@ionic-native/google-maps";
 import { KitchenSinkPage } from "../pages/kitchen-sink/kitchen-sink";
+import { IonicStorageModule } from "@ionic/storage";
 
 @NgModule({
   declarations: [
@@ -45,6 +46,7 @@ import { KitchenSinkPage } from "../pages/kitchen-sink/kitchen-sink";
     MapPage,
     ConnexionPage,
     KitchenSinkPage,
+    IonicStorageModule
   ],
   imports: [BrowserModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
@@ -65,6 +67,7 @@ import { KitchenSinkPage } from "../pages/kitchen-sink/kitchen-sink";
     MapPage,
     ConnexionPage,
     KitchenSinkPage,
+    IonicStorageModule
   ],
   providers: [
     StatusBar,
