07bc8ba9ef23fe4ed52140fa96e9a2617974b199
Nadir BERKOUN <nberkoun@orchestra-premaman.com>
2019-01-11 11:26:56 +0100

    ajout files

---
+++ src/pages/home-chat/home-chat.module.ts
@@ -0,0 +1,14 @@
+/*import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { HomeChatPage } from './home-chat';
+
+@NgModule({
+  declarations: [
+    HomeChatPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(HomeChatPage),
+  ],
+})
+export class HomeChatPageModule {}
+*/
