aebede362c54d023ec19d8448ddf008b5677db4a
CLEMENT CROS <clement.cros@sequoiasoft.com>
2019-01-11 09:48:50 +0100

    map master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,26 +10,38 @@ import { CustomHeader } from "../pages/custom-header/custom-header";
 import { Inscription } from "../pages/inscription/inscription";
 import { InscriptionMailPage } from "../pages/inscription-mail/inscription-mail";
 import { ConnexionPage } from "../pages/connexion/connexion";
-import { ConnexionMailPage } from "../pages/connexion-mail/connexion-mail";
-
-import { EventPicker } from "../pages/eventPicker/eventPicker";
 import { PhotoHandler } from "../pages/photoHandler/photoHandler";
-
-import { StatusBar } from "@ionic-native/status-bar";
-import { SplashScreen } from "@ionic-native/splash-screen";
+import { EventPicker } from "../pages/eventPicker/eventPicker";
+import { ConnexionMailPage } from "../pages/connexion-mail/connexion-mail";
 import { MapPage } from "../pages/map/map";
-
-import { Camera } from "@ionic-native/camera";
-import { Geolocation } from "@ionic-native/geolocation";
 import { listSportPage } from "../pages/listSport/listSport";
 import { surfPage } from "../pages/surf/surf";
 import { skiPage } from "../pages/Ski/ski";
 import { randoPage } from "../pages/rando/rando";
-import { GoogleMaps } from "@ionic-native/google-maps";
 import { KitchenSinkPage } from "../pages/kitchen-sink/kitchen-sink";
-import { IonicStorageModule } from "@ionic/storage";
-import { globalServices } from "../pages/services/globalServices";
+// import { globalServices } from "../pages/services/globalServices";
 import { confidentialityPage } from "../pages/confidentiality/confidentiality";
+
+// Module supp ionic
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { Camera } from "@ionic-native/camera";
+import { Geolocation } from "@ionic-native/geolocation";
+import { IonicStorageModule } from "@ionic/storage";
+import { StatusBar } from "@ionic-native/status-bar";
+import {AngularFireModule} from "angularfire2";
+import {AngularFirestoreModule} from "angularfire2/firestore";
+import {MarkersProvider} from "../providers/markers/markers";
+import { NativeGeocoder } from '@ionic-native/native-geocoder';
+import {LocationAccuracy} from "@ionic-native/location-accuracy";
+
+var config = {
+  apiKey: "<yourapikeyhere>",
+  authDomain: "<armada-dev-1546938684577.firebaseapp.com>",
+  databaseURL: "<map>",
+  projectId: "<projectIdhere>",
+  storageBucket: "<oLiKzi3DSYaOiPhqDlW5>",
+  messagingSenderId: "<yourmsgingsenderidhere>"
+};
 
 @NgModule({
   declarations: [
@@ -50,10 +62,13 @@ import { confidentialityPage } from "../pages/confidentiality/confidentiality";
     ConnexionPage,
     KitchenSinkPage,
     confidentialityPage,
+    // globalServices,
     ConnexionMailPage
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule,
     IonicModule.forRoot(MyApp),
     IonicStorageModule.forRoot()
   ],
@@ -83,8 +98,9 @@ import { confidentialityPage } from "../pages/confidentiality/confidentiality";
     SplashScreen,
     Camera,
     Geolocation,
-    GoogleMaps,
-    globalServices,
+    LocationAccuracy,
+    NativeGeocoder,
+    MarkersProvider,
     { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
